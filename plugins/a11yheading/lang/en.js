4c94d6b9c4fe9bd412fb58745d8d97e6c44a9761
Nicholas Hoyt <nhoyt@illinois.edu>
2018-06-21 11:41:16 -0500

    Initial commit for new distribution

---
+++ plugins/a11yheading/lang/en.js
@@ -0,0 +1,19 @@
+/**
+* Copyright (c) 2018 University of Illinois - Jon Gunderson and Nicholas Hoyt. All rights reserved.
+* For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'a11yheading', 'en', {
+  label:          'Heading / Paragraph',
+  panelTitle:     'Select heading level',
+  level_h1:       'H1 – Document title',
+  level_h2:       'H2 – Section title',
+  level_h3:       'H3 – Subsection title',
+  level_h4:       'H4 – Subsection title',
+  level_h5:       'H5 – Subsection title',
+  level_h6:       'H6 – Subsection title',
+  format_p:       'Normal',
+  format_pre:     'Preformatted text',
+  format_address: 'Address line',
+  format_div:     'Normal (DIV)',
+  helpLabel:      'Help'
+} );
