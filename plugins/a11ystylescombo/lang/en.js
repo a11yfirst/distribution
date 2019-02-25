079b245236c0cf774637f9db3a5ea36753af2261
Nicholas Hoyt <nhoyt@illinois.edu>
2019-02-25 14:31:00 -0600

    Initial commit for new distribution

---
+++ plugins/a11ystylescombo/lang/en.js
@@ -0,0 +1,15 @@
+/**
+* Copyright (c) 2018 University of Illinois - Jon Gunderson and Nicholas Hoyt. All rights reserved.
+* For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'a11ystylescombo', 'en', {
+  label:             'Character Style',
+  helpLabel:         'Help',
+  removeStylesLabel: 'Remove styles',
+  panelTitle:        'Select character style',
+  panelTitle1:       'Block Styles',
+  panelTitle2:       'Inline Styles',
+  panelTitle3:       'Object Styles',
+  helpNotFound:      'The A11yFirst Help system is not installed. Please contact \
+your CMS or CKEditor administrator for more information.'
+} );
